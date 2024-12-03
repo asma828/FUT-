2c1d5d42d1d27115a26e11594abc33ea31b03cbb
asma828 <asmalachhab66@gmail.com>
2024-12-03 13:43:04 +0100

    add burger menu function

--- js/main.js
+++ js/main.js
@@ -813,3 +813,23 @@ let createPlayerPop = document.getElementById("createPop");
     onCloseCreatePlayer();
   });
   
+
+   // burger menu
+   const menuButton = document.getElementById('menu-button');
+   const menu = document.getElementById('menu');
+   
+   menuButton.addEventListener('click', () => {
+       if (menu.classList.contains('hidden')) {
+           menu.classList.remove('hidden');
+           setTimeout(() => {
+               menu.classList.remove('-translate-x-full');
+               menu.classList.add('translate-x-0');
+           }, 10); 
+       } else {
+           menu.classList.remove('translate-x-0');
+           menu.classList.add('-translate-x-full');
+           setTimeout(() => {
+               menu.classList.add('hidden');
+           }, 300); 
+       }
+   });
